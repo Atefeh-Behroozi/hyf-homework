e616954ac229a1abe28b2b00dbc8542ebbe38ff9
Atefeh <ate.behroozi@gmail.com>
2023-06-13 18:44:05 +0200

    Add arguments and numbers

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,5 +1,13 @@
 const args = process.argv.slice(2);
+if (args.length < 2) {
+    console.log("Please provide at least two arguments.");
+    process.exit(1);
+}
 const numbers = args.map(Number);
+if (numbers.some(isNaN)) {
+    console.log("One or more arguments are not valid numbers.");
+    process.exit(1);
+  }
 const sum = numbers.reduce((acc, num) => acc + num, 0);
 const average = sum / numbers.length;
 console.log(average);
