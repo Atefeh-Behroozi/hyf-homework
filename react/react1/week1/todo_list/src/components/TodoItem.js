0f2036e8d7c9cf610b8f4cb4e154b20001ce4096
Atefeh <ate.behroozi@gmail.com>
2023-06-30 12:12:27 +0200

    add new files

---
+++ react/react1/week1/todo_list/src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoItem(props) {
+  return (
+    <div>
+      <h3>{props.description}</h3>
+      <p>{props.deadline}</p>
+    </div>
+  );
+}
+
+export default TodoItem;
